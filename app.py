ea2ca883121c0f3a6ca3a1e9bb7b6a06ba663895
Divyanshu Prakash <divyanshu2727@gmail.com>
2024-08-27 10:32:32 +0530

    app file added

---
+++ app.py
@@ -0,0 +1,15 @@
+# app file
+
+import cal
+
+
+
+print("if you want to use calculator then press 1 otherwise press 0 to exit")
+
+input_keyword = int(input("enter the keyword: ")
+
+if input_keyword == 1:
+	output = cal()
+
+else:
+	print("exit")
